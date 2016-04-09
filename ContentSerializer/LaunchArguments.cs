260741ffabda43adb2c799f5934214e78a8b85cc
Mythic Maniac <mythicmaniac@gmail.com>
2016-04-09 20:40:21 +0300

    Initial commit
    Initial project commit

---
+++ ContentSerializer/LaunchArguments.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueSandbox.ContentSerializer
+{
+    public class LaunchArguments
+    {
+        private Dictionary<string, string> _arguments;
+
+        private LaunchArguments() { _arguments = new Dictionary<string, string>(); }
+
+        /// <summary>
+        /// Returns the argument for given key
+        /// </summary>
+        /// <param name="key">Argument key</param>
+        /// <returns></returns>
+        public string this[string key]
+        {
+            get { return _arguments[key]; }
+        }
+
+        /// <summary>
+        /// Returns wether or a provided key exists
+        /// </summary>
+        /// <param name="key">Key which existance to check</param>
+        /// <returns></returns>
+        public bool ContainsKey(string key)
+        {
+            return _arguments.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// Parses launch arguments into a key/value mapping
+        /// </summary>
+        /// <param name="arguments">Launch arguments</param>
+        /// <returns>LaunchArguments object</returns>
+        public static LaunchArguments Parse(string[] arguments)
+        {
+            var result = new LaunchArguments();
+
+            //Make sure we have a proper amount of arguments, since the format is --<key> <value>
+            if (arguments.Length % 2 != 0) throw new Exception("Invalid launch argument count");
+
+            //Form key value pairs
+            for (var i = 0; i < arguments.Length;)
+            {
+                var key = arguments[i++];
+                var value = arguments[i++];
+
+                //Make sure proper format is employed
+                if (!key.StartsWith("--") || key.Contains(' ')) throw new Exception("Invalid argument format");
+                if (key.Length < 3) throw new Exception("Invalid argument key length");
+                if (key[2] == '-') throw new Exception("Invalid argument key");
+
+                //Trim leading dashes
+                key = key.TrimStart('-');
+
+                result._arguments.Add(key, value);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ContentSerializer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueLib.Tools;
+using System.IO;
+
+namespace LeagueSandbox.ContentSerializer
+{
+    public class Program
+    {
+        static string GetRadsPath(LaunchArguments arguments)
+        {
+            if (arguments.ContainsKey("radsPath")) return arguments["radsPath"];
+            if (File.Exists("rads-path.txt")) return File.ReadAllText("rads-path.txt");
+
+            throw new Exception("No RADS path defined");
+        }
+
+        static void Main(string[] args)
+        {
+            var arguments = LaunchArguments.Parse(args);
+            var radsPath = GetRadsPath(arguments);
+
+            var manager = new ArchiveFileManager(radsPath);
+            var fileEntries = manager.GetAllFileEntries().OrderBy(x => x.FullName);
+
+            foreach (var entry in fileEntries)
+            {
+                Console.WriteLine(entry.FullName);
+            }
+        }
+    }
+}
