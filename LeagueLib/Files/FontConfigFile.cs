5ba970298cdc8278e85182fbaf62766dede5d03f
Mythic Maniac <mythicmaniac@gmail.com>
2016-04-13 02:33:39 +0300

    Add FontConfigFile to LeagueLib
    Add a font config (AKA localization file) parser to LeagueLib

---
+++ LeagueLib/Files/FontConfigFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueLib.Files
+{
+    public partial class FontConfigFile
+    {
+        public Dictionary<string, string> Content { get; private set; }
+        private List<string> _header;
+
+        private FontConfigFile()
+        {
+            Content = new Dictionary<string, string>();
+            _header = new List<string>();
+        }
+    }
+}
